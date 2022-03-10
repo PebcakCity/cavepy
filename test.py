24a1f39d7500f6494eeef38ea9eb79e424245f5e
tannmatter <tannmatter@gmail.com>
2022-03-10 14:56:33 -0600

    replace ActionSpinner with swipe behavior

--- test.py
+++ test.py
@@ -6,27 +6,53 @@ from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+from kivy.properties import NumericProperty
 from kivy.properties import StringProperty
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
 
 
-# Todo - find a reliable way to determine whether this is running on a Raspberry Pi
-Window.fullscreen = True
+# Todo 1) Find a reliable way to determine whether this is running on a Raspberry Pi
+#      so that Window.fullscreen or Window.size can be set automatically.
+# Window.fullscreen = True
+Window.size = (1024, 480)
+
+SWIPE_THRESHOLD = 20
 
 
 class MyAccordion(Accordion):
     def __init__(self, **kwargs):
         self.current_tab_name = ''
+        self.initial = 0
         super(MyAccordion, self).__init__(**kwargs)
+
+    def on_touch_down(self, touch):
+        self.initial = touch.x
+        return super(MyAccordion, self).on_touch_down(touch)
+
+    def on_touch_up(self, touch):
+        app = App.get_running_app()
+        idx = app.available_screens.index(app.current_screen_name)
+        # Swiping right - move a tab to the left
+        if touch.x - self.initial > SWIPE_THRESHOLD:
+            if idx > 0:
+                app.go_screen(idx - 1)
+        # Swiping left - move a tab to the right
+        elif self.initial - touch.x > SWIPE_THRESHOLD:
+            if idx < len(app.available_screens) - 1:
+                app.go_screen(idx + 1)
+        return super(MyAccordion, self).on_touch_up(touch)
 
 
 class MyAccordionItem(AccordionItem):
+    def __init__(self, **kwargs):
+        super(MyAccordionItem, self).__init__(**kwargs)
+
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos):
             self.accordion.current_tab_name = self.title
             app = App.get_running_app()
-            app.current_tab_name = self.title
+            app.current_screen_name = self.title
 
         return super(MyAccordionItem, self).on_touch_down(touch)
 
@@ -40,7 +66,7 @@ class MyInputButton(Button):
 
     def on_release(self):
         app = App.get_running_app()
-        tab = app.current_tab_name
+        tab = app.current_screen_name
         popup = app.root.ids['popup']
         popup_label = app.root.ids['popup_label']
         popup.title = tab
@@ -58,7 +84,7 @@ class TestRootWidget(BoxLayout):
         acc = MyAccordion()
         self.ids['accordion'] = acc
         acc.orientation = 'horizontal'
-        for idx, tab_name in enumerate(self.app.available_tabs):
+        for idx, tab_name in enumerate(self.app.available_screens):
             item = MyAccordionItem(title=tab_name)
             self.ids['accordion_item_'+str(idx)] = item
             fl = FloatLayout()
@@ -83,16 +109,16 @@ class TestRootWidget(BoxLayout):
 
 
 class TestApp(App):
-    current_tab_name = StringProperty()
+    current_screen_name = StringProperty()
 
     def __init__(self):
-        self.available_tabs = ['Home', 'Projector', 'Switcher']
+        self.available_screens = ['Home', 'Projector', 'Switcher']
         super(TestApp, self).__init__()
 
     def build(self):
         return TestRootWidget(self)
 
-    def go_tab(self, idx):
+    def go_screen(self, idx):
         # Make sure the root window has been created (meaning App.build has returned)
         if self.root is not None:
             item = self.root.ids['accordion_item_'+str(idx)]
